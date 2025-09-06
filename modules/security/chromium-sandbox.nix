466c74530828894ae0727a219517841ede9029f4
Silver <silver@squirl.dev>
2025-09-06 16:25:22 +0100

    improve chromium sandbox module

--- modules/security/chromium-sandbox.nix
+++ modules/security/chromium-sandbox.nix
@@ -1,11 +1,29 @@
-{pkgs, ...}: let
-  sandbox = pkgs.chromium.sandbox;
-  name = sandbox.passthru.sandboxExecutableName;
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.security.chromium-sandbox;
+  name = cfg.package.passthru.sandboxExecutableName;
 in {
-  security.wrappers.${name} = {
-    setuid = true;
-    owner = "root";
-    group = "root";
-    source = "${sandbox}/bin/${name}";
+  sandbox = pkgs.chromium.sandbox;
+  options.security.chromium-sandbox = {
+    enable = lib.mkEnableOption "chromium SUID sandbox wrapper";
+    package = lib.mkOption {
+      types = lib.types.package;
+      default = pkgs.chromium.sandbox;
+      defaultString = lib.literalExpression "pkgs.chromium.sandbox";
+      description = "The chromium sandbox package to use";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    security.wrappers.${name} = {
+      setuid = true;
+      owner = "root";
+      group = "root";
+      source = "${cfg.package}/bin/${name}";
+    };
   };
 }
